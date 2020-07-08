48a59d0cf678babc20169ef92c7e07eb34196510
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-07-08 14:46:18 +0300

    Initial file uploads

---
+++ main.js
@@ -0,0 +1,35 @@
+(function (){const  quotes=[
+{quote:'\"The purpose of our lives is to be happy\"',
+author:'\-Dalai Lama'
+},
+{
+quote:'\"Life is what happens when you are busy making other plans\"',
+author:'\-John Lennon'
+},
+{
+quote:'\"Get busy living or get busy dying\"',
+author:'\-Stephen King'
+},
+{
+quote:'\"You only live once, but if you do it right, once is enough\"',
+author:'\-Mae West'
+},
+{
+quote:'\"If you want to live a happy life, tie it to a goal, not to people or things\"',
+author:'\-Albert Einstein'
+}
+]
+
+//constants
+const para=document.getElementById('quoteHolder');
+const button=document.querySelector('button');
+
+button.addEventListener('click',changeQuote)
+
+function changeQuote(){
+	let randomQuotes=Math.floor(Math.random()*quotes.length);
+	para.textContent=quotes[randomQuotes].quote;
+	document.getElementById('author').textContent=quotes[randomQuotes].author;
+}
+})();
+
